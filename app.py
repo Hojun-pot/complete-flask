f0be595b54739cfa7f09c82fa520b70f30f7f6d7
Hojun-pot <110947741+Hojun-pot@users.noreply.github.com>
2022-12-15 18:48:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ def read(cursor):
         Result['Name'] = row[0].replace('\n', ' ')
         Result['Email'] = row[1]
         Result['ID'] = row[2]
-        html = html + (f"<tr style='border: 1px solid blue'><th  style='border: 1px solid blue'>{Result['Name']}</th> <th style='border: 1px solid blue'>{Result['Email']}</th></tr> <br>")
+        html = html + (f"<tr style='border: 1px solid red'><th  style='border: 1px solid red'>{Result['Name']}</th> <th style='border: 1px solid red'>{Result['Email']}</th></tr> <br>")
 
-    html = f"<table style='border: 1px solid blue'><tr style='border: 1px solid blue'><th  style='border: 1px solid blue'>Name</th><th style='border: 1px solid blue'>Email</th></tr>{html}</table>"
+    html = f"<table style='border: 1px solid red'><tr style='border: 1px solid red'><th  style='border: 1px solid red'>Name</th><th style='border: 1px solid red'>Email</th></tr>{html}</table>"
     # response = {'Results': Results, 'count': len(Results)}
     # ret = app.response_class(
     #     response=json.dumps(response),
